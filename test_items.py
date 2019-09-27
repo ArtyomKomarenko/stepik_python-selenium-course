0413b76110a20a8dbbdf02a45493f97a11197d54
Artyom Komarenko <artyom.komarenko@mail.ru>
2019-09-27 08:44:56 +0300

    Rename test

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-def test_add_to_cart_button(browser):
+def test_find_add_to_cart_button(browser):
     url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(url)
     button = browser.find_element_by_css_selector('.btn-add-to-basket')
