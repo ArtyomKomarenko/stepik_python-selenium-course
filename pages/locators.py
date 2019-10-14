61f74db15c200a827171cacc96c3514cf527881f
Artyom Komarenko <artyom.komarenko@mail.ru>
2019-10-14 23:15:39 +0300

    Add product page object and tests

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,24 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class MainPageLocators:
+    pass
 
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    ITEM_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    ITEM_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages  .alert-success')
+    SUCCESS_MESSAGE__ITEM_NAME = (By.CSS_SELECTOR, '#messages  .alert-success .alertinner strong')
+    INFO_MESSAGE__PRICE = (By.CSS_SELECTOR, '#messages  .alert-info .alertinner strong')
---
+++ pages/product_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_item_to_cart(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        button.click()
+
+    def get_item_price(self):
+        return self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+
+    def get_item_name(self):
+        return self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+
+    def should_be_info_message_with_price(self, price):
+        message_price = self.browser.find_element(*ProductPageLocators.INFO_MESSAGE__PRICE).text
+        assert price == message_price, f'Price "{message_price}" in message is not equal to item price "{price}"'
+
+    def should_be_succes_message_with_item_name(self, name):
+        message_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE__ITEM_NAME).text
+        assert name == message_name, f'Item name "{message_name}" in message is not equal to item name "{name}"'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            'Success message is presented, but should not be'
